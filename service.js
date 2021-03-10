15c710cadeb064c05d887891c38db1455aa4ce8f
ArtemDid <temadidenko27@gmail.com>
2021-03-10 19:07:55 +0200

    //21

--- service.js
+++ service.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     host: '0.tcp.ngrok.io' , // postgres://ztcjytohfqbrnk:e9fbda1f66ad087d9ae21bc906f5e7db3bb0d99830e5adf254da1411fa24166d@ec2-54-73-147-133.eu-west-1.compute.amazonaws.com:5432/deq0a3s66rcipk
     database: 'history',
     password: '123456',
-    port: 13587,
+    port: 5432,
     ssl: false
 });
 
